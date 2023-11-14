14c3f338e9db79efb05899551895a20bc047c5a4
Ismael Escalante <ismael.escalante@bosonit.com>
2023-11-14 09:27:39 +0100

    update

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,12 +5,14 @@ import { LayoutComponent } from './layout/layout.component';
 import { MainRoutingModule } from './main-routing.module';
 import { NgModule } from '@angular/core';
 import { PrimengModule } from '../primeng/primeng.module';
+import { ProfileComponent } from './features/profile/profile.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     DashboardComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
